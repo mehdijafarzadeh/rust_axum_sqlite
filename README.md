a617efd4b31ea728693a43f51161feba7fca2cad
Mehdi Jafarzadeh <jafarzade.me@gmail.com>
2025-01-17 11:32:21 +0100

    update the database connection

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rust_axum_sqlite
+
+> Crud example of [axum](https://github.com/tokio-rs/axum) and [sqlite](https://github.com/rusqlite/rusqlite) 
+> using [sqlx](https://github.com/launchbadge/sqlx).
+```bash
+cargo run
+```
--- src/db.rs
+++ src/db.rs
@@ -1,23 +1,30 @@
 
-use sqlx::{sqlite::SqlitePoolOptions, Pool, Sqlite};
+use sqlx::{Pool, Sqlite};
 use std::env;
 use dotenvy::dotenv;
 use sqlx::migrate::MigrateDatabase;
+use sqlx::SqlitePool;
+
 
 pub async fn connect_db() -> Result<Pool<Sqlite>, sqlx::Error> {
     dotenv().ok();
-    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
+    let db_url = env::var("DATABASE_URL").unwrap();
+    
     // Check if the database exists, and create it if it doesn't
     if !Sqlite::database_exists(&db_url).await.unwrap_or(false) {
         println!("Creating database {}", &db_url);
         Sqlite::create_database(&db_url).await?;
+
+        // If the database is newly created, run the migrations
+        let pool = SqlitePool::connect(&db_url).await?;
+        sqlx::migrate!("./migrations").run(&pool).await?;
+        Ok(pool)
     } else {
         println!("Database already exists");
+        
+        // If the database already exists, just connect to it without running migrations
+        let pool = SqlitePool::connect(&db_url).await?;
+        Ok(pool)
     }
-    let pool  = SqlitePoolOptions::new()
-        .max_connections(5)
-        .connect(&db_url)
-        .await?;
-    Ok(pool)
 }
